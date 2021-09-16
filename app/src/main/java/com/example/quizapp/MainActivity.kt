16e2217e96ce38a901ee3103a09998349ee36711
william.lundqvist@iths.se <william.lundqvist@iths.se>
2021-09-16 13:05:43 +0200

    initial commit

--- app/src/main/java/com/example/quizapp/MainActivity.kt
+++ app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.example.quizapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val button = findViewById<Button>(R.id.startButton)
+
+        button.setOnClickListener {
+            startCategoryQuestionActivity()
+        }
+
+    }
+
+
+     fun startCategoryQuestionActivity() {
+
+         Log.e("!!!", "hej")
+
+        val intent = Intent(this, CategoryQuestionActivity::class.java)
+        startActivity(intent)
+
     }
 }
